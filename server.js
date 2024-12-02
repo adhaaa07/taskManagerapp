91b2714ae646576a6faf0563c680235b42401e56
adhaan <adhaan000@gmail.com>
2024-12-02 13:27:39 +0100

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const taskRoute = require("./routes/task.routes");
 const userRoute = require("./routes/user.routes");
 const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
+
 
 // const port = 5000;
 const port = process.env.PORT || 3000;
@@ -16,6 +16,7 @@ const uri = process.env.MONGO_URI
 // Middleware
 app.use(cors({ origin: "*" }));
 app.use(express.json()); // Parse JSON requests
+app.use(express.static(path.join(__dirname, "public")));
 
 
 
